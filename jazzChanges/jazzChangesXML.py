02c17a7f69f28b06523ecf3cf9a311546d9ac2b5
b-cheek <cheekbw@gmail.com>
2024-12-26 03:02:36 -0500

    Capture metadata, account for basic repeat

--- jazzChanges/jazzChangesXML.py
+++ jazzChanges/jazzChangesXML.py
@@ -2,19 +2,43 @@ import xml.etree.ElementTree as ET
 import os
 
 # Parse xml files in MusicXML Lead Sheets folder
-for filename in os.listdir('jazzChanges\MusicXML Lead Sheets'):
-    tree = ET.parse('jazzChanges\MusicXML Lead Sheets/' + filename)
+for filename in os.listdir('jazzChanges/MusicXML Lead Sheets'):
+    tree = ET.parse('jazzChanges/MusicXML Lead Sheets/' + filename)
     root = tree.getroot()
+    # Get metadata
+    title = root.find('.//work-title').text
+    composer = root.find('.//creator[@type="composer"]').text
+    style = root.find('.//creator[@type="lyricist"]').text
+
+    key = root.find('.//fifths').text + ' ' + root.find('.//mode').text
+
     # Create list of measures
     measures = []
     chords = []
     chord_names = []
-    for measure in tree.iter('measure'):
+    for measure in root.iter('measure'):
         measures.append(measure)
 
-    for measure in measures:
-        chords += measure.findall('harmony')
+    cur_measure = 0
+    repeat_to = 0
+    take_repeat = True
+    while cur_measure < len(measures):
+        if measures[cur_measure].find('./barline[@location="left"]/repeat[@direction="forward"]') is not None:
+            repeat_to = cur_measure
+        chords += measures[cur_measure].findall('harmony')
+
+        if measures[cur_measure].find('./barline[@location="right"]/repeat[@direction="backward"]') is not None:
+            if take_repeat:
+                cur_measure = repeat_to
+                take_repeat = False
+            else:
+                cur_measure += 1
+                take_repeat = True
+
+        else:
+            cur_measure += 1
     
     for chord in chords:
         chord_names.append(chord.find('kind').text)
+    
     print(filename)
