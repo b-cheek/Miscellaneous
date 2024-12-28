606cde236e00d8787df71fcf3b7d84279dbfb51a
b-cheek <cheekbw@gmail.com>
2024-12-28 04:46:36 -0500

    Add functionality for logging chord deltas

--- jazzChanges/jazzChangesXML.py
+++ jazzChanges/jazzChangesXML.py
@@ -1,5 +1,23 @@
 import xml.etree.ElementTree as ET
 import os
+from collections import Counter
+
+counters = dict()
+
+semitone_map = {'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11}
+
+def get_semitone(chord):
+    return semitone_map[chord.find('./root/root-step').text]\
+         + int(chord.find('./root/root-alter').text)
+
+def track_changes(chord_list):
+    for i in range(len(chord_list) - 1):
+        quality = chord_list[i].find('kind').text
+        interval = (get_semitone(chord_list[i + 1]) - get_semitone(chord_list[i])) % 12
+        next_quality = chord_list[i + 1].find('kind').text
+        if quality not in counters:
+            counters[quality] = Counter()
+        counters[quality][(interval, next_quality)] += 1
 
 # Parse xml files in MusicXML Lead Sheets folder
 for filename in os.listdir('jazzChanges/MusicXML Lead Sheets'):
@@ -17,7 +35,6 @@ for filename in os.listdir('jazzChanges/MusicXML Lead Sheets'):
     chords = [] # each chord in the order it OCCURS, accounting for repeats, dc, etc
     ending_chords = [] # separate list for an ending that only occurs last time through (preceding chord + coda)
     # Note that for now I am doing a naive implementation to just get chords after final coda due to coda inconsistencies (see personal notes)
-    chord_names = []
     for measure in root.iter('measure'):
         measures.append(measure)
 
@@ -31,7 +48,10 @@ for filename in os.listdir('jazzChanges/MusicXML Lead Sheets'):
             # Will work or non-ending roadmap stuff first (ds, dc, 1/2 ending)
         if measures[cur_measure].find('./barline[@location="left"]/repeat[@direction="forward"]') is not None:
             repeat_to = cur_measure
-        chords += measures[cur_measure].findall('harmony')
+        chords += filter(
+            lambda chord: chord.find('kind').text != 'none',
+            measures[cur_measure].findall('harmony')
+        )
 
         if measures[cur_measure].find('./barline[@location="right"]/repeat[@direction="backward"]') is not None:
             if take_repeat:
@@ -44,7 +64,15 @@ for filename in os.listdir('jazzChanges/MusicXML Lead Sheets'):
         else:
             cur_measure += 1
     
-    for chord in chords:
-        chord_names.append(chord.find('kind').text)
+    # Add chord progression to counters
+    track_changes(chords)
     
     print(filename)
+
+# Write counter data to a csv file, with separate data sets with interval and next_quality joined and not joined
+with open('jazzChanges/counter_data.csv', 'w') as f:
+    f.write('quality,interval + next_quality,count,,quality,interval,next_quality,count\n')
+    for quality in counters:
+        for (interval, next_quality) in counters[quality]:
+            f.write(quality + ',' + str(interval) + ' ' + next_quality + ',' + str(counters[quality][(interval, next_quality)]) + ',,'\
+                  + quality + ',' + str(interval) + ',' + next_quality + ',' + str(counters[quality][(interval, next_quality)]) + '\n')
